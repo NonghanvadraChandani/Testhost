13b096c01e6f5794a023a84747b7d8530ea9dfca
chandani nonghanvadra <91254443+NonghanvadraChandani@users.noreply.github.com>
2022-06-22 17:49:55 +0530

    patch delete added

--- route.js
+++ route.js
@@ -6,8 +6,53 @@ var Movie = require('./Models/Movie');
 //     res.sendFile("index.html", { root: __dirname });
 // });
 
+//fetching data
 router.get('/movies', async(req, res) => {
     const iMovie = await Movie.find();
     res.send(iMovie);
 })
+
+//insert data
+router.post('/movies', async(req, res) => {
+    const iMovie = new Movie({
+        name: req.body.name,
+        rating: req.body.rating
+    })
+    console.log(iMovie);
+    await iMovie.save((err, msg) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(msg);
+        }
+    });
+})
+
+//updating data
+
+router.patch('/movies/:id', async(req, res) => {
+    const iMovie = await Movie.findOne({ _id: req.params.id });
+    iMovie.name = req.body.name;
+    iMovie.rating = req.body.rating;
+
+    await iMovie.save((err, msg) => {
+        if (err) {
+            res.status(500).json({ "err": err });
+        } else {
+            res.status(200).json({ "message": msg });
+        }
+    });
+})
+
+//deleting data
+router.delete("/movies/:id", async(req, res) => {
+    await Movie.deleteOne({ _id: req.params.id }, (err, msg) => {
+        if (err) {
+            res.status(500).json({ "err": err });
+        } else {
+            res.status(200).json({ "message": msg });
+        }
+    })
+})
+
 module.exports = router;
